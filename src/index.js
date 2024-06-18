0c06d4a542be935127702f3ba6da02c38b624d91
Rolay <rolay8@gmail.com>
2024-06-18 10:41:23 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.luouo.com": dockerHub,
+  "quay.luouo.com": "https://quay.io",
+  "gcr.luouo.com": "https://gcr.io",
+  "k8s-gcr.luouo.com": "https://k8s.gcr.io",
+  "k8s.luouo.com": "https://registry.k8s.io",
+  "ghcr.luouo.com": "https://ghcr.io",
+  "cloudsmith.luouo.com": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.luouo.com": dockerHub,
 };
 
 function routeByHosts(host) {
